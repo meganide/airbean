781531aac37f93caca8d41cd29a9fa91184ff3d0
Meganide <renashassan@hotmail.com>
2023-02-27 15:36:19 +0100

    adedd animation on logo

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,11 +1,30 @@
-import "./landing.scss"
+import './landing.scss';
 
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import Logo from '../../components/Logo/Logo';
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     // navigate('/menu');
+  //   }, 2000);
+  // }, []);
+
+
+
   return (
-    <h1 className="heading">ehehjdfjgjd</h1>
-  )
+    <header className="landing">
+      <img className="landing__bg" src="./assets/pictures/header_left.svg" alt="" />
+      <img className="landing__bg landing__bg--right" src="./assets/pictures/header_right.svg" alt="" />
+      <Logo />
+      <h1 className="landing__heading">AIR BEAN</h1>
+      <h2 className="landing__slogan">DRONEDELIVERED COFFEE</h2>
+    </header>
+  );
 }
 
-export default Landing
+export default Landing;
