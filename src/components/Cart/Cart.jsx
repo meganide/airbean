6c224ca862554e446f9b8b7ed9d04d253b3a2749
Diana Hogescu <93951152+BubbleGummi@users.noreply.github.com>
2023-02-28 14:18:04 +0100

    PopUp-button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,40 @@
-import "./Cart.scss";
-import React from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Cart() {
+function Popup(props) {
   return (
-    <div className="cart">
-      <div className="cart__heading">
-        <h1>Din beställning</h1>
-      </div>
-      <div className="cart__list">
-        <ul>
-          <li>kaffe</li>
-        </ul>
+    <div className="popup">
+      <div className="popup-inner">
+        <h2>Your Order</h2>
+        <p>Order: {props.order}</p>
+        <p>Total Amount: {props.totalAmount}</p>
+        <button onClick={props.closePopup}>Close</button>
       </div>
     </div>
   );
 }
 
+function Cart() {
+  const [showPopup, setShowPopup] = useState(false);
+  const order = "Example Order";
+  const totalAmount = 100;
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  }
+
+  return (
+    <div className="app">
+      <button onClick={togglePopup}>Show Popup</button>
+      {showPopup && 
+        <Popup 
+          order={order} 
+          totalAmount={totalAmount} 
+          closePopup={togglePopup}
+        />
+      }
+    </div>
+  );
+}
 
 export default Cart;
